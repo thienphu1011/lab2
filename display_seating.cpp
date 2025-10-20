377a9ceb9a84f023a181aced816e700a365916ad
thienphu1011 <phuthien10112006@gmail.com>
2025-10-20 10:42:32 +0700

    Fix blank space of seats

--- display_seating.cpp
+++ display_seating.cpp
@@ -7,29 +7,24 @@ using namespace std;
 
 
 
-
-void displaySeating(bool seats[ROWS][COLS]) {
-    cout << "\n\t\tSeats\n        ";
-    for (int i=1; i<=3; i++){
-    for (int c = 1; c <= 10; ++c) {
-        if (c==10)
-         cout <<"0"<<"";
-        
-    
-        else cout  << c;
+void displaySeating(bool seats[ROWS][COLS])
+{
+    cout << setw(27) << "Seats\n";
+    cout << setw(8) << " ";
+    for(int c = 1; c <= COLS; c++)
+    {
+        cout << (c % 10);
     }
-    
-    }
-
     cout << endl;
-
-    for (int r = 0; r < ROWS; ++r) {
-        cout << "Row " << setw(2) << (r + 1) << "  ";
-        for (int c = 0; c < COLS; ++c) {
-            char ch = seats[r][c] ? '#' : '#';
-            cout <<  ch;
+    for (int r = 0; r < ROWS; r++)
+    {
+        cout << "Row" << setw(3) << (r + 1) << "  ";
+        for(int c = 0; c < COLS; c++)
+        {
+            char ch = seats[r][c]? '#' : '*';
+            cout << ch;
         }
         cout << endl;
     }
+    
 }
- 
